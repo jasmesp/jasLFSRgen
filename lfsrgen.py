48ac148f3d8e008262f19a3bb7ff542ce76efa1e
jasmesp <jasmes@gmail.com>
2021-10-06 19:52:55 -0400

    big fix

--- lfsrgen.py
+++ lfsrgen.py
@@ -1,30 +1,34 @@
-def form(arg):
-    print("{:04b}".format(arg))
+
+def quit_or_new():
+	quit = (input("\nQ to exit, N to continue:\n")).lower()
+	if quit == "n":
+		menu()
+	else:
+		pass
+
+def menu():
+	mode = int(input("\nEnter how many thousand digits, or '0' for continuous output:\n"))
+	if mode == 0:
+		infinite_output()
+	else:
+		set_output(mode*1000)
+
+def infinite_output():
+	rout = 1 << 127 | 1
+	while True:
+		print(rout & 1, end = "")
+		shiftval = (rout) ^ (rout >> 1) ^ (rout >> 2) ^ (rout >> 7)
+		rout = rout >> 1 | shiftval << 127
+
+def set_output(charlimit):
+	rout = 1 << 127 | 1
+	for i in range(charlimit):
+		print(rout & 1, end = "")
+		shiftval = (rout) ^ (rout >> 1) ^ (rout >> 2) ^ (rout >> 7)
+		rout = rout >> 1 | shiftval << 127
+	quit_or_new()
 
 
-def test():
-    IV = 0b1101
-
-    for i in range(16):
-        new_bit = (IV ^ (IV >> 1))
-        IV = (IV >> 1) | (new_bit << 3)
-        print(IV & 1, end="")
 
 
-def main():
-    output_length = int(input("How many digits?\n"))
-    iv = (1 << 127) | 1
-    for i in range(output_length):
-        s_bit = (iv ^ (iv >> 1) ^ (iv >> 2) ^ (iv >> 7))
-        iv = (s_bit >> 1) | (iv << 3)
-        print(iv & 1, end="")
-    print("")
-    quit = (input("Q to exit, N to continue:\n")).lower()
-
-    if quit == ("q"):
-        pass
-    else:
-        main()
-
-
-main()
+menu()
